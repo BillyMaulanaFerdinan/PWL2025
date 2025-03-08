82cb43c127722a28a6c4fd8b37afe471e9212c0f
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2025-03-08 20:38:50 +0700

    Meneruskan data ke view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,6 +11,8 @@ class WelcomeController extends Controller
         }
 
     public function greeting(){
-        return view('blog.hello', ['name' => 'Andi']);
+        return view('blog.hello')
+        ->with('name','Billy')
+        ->with('occupation','Mahasiswa');
         }
 }
